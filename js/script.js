9c20954f8276a9662497a42dee91c12f330da099
albertotieno41@gmail.com <albertotieno41@gmail.com>
2020-07-24 14:12:40 +0300

    ADDED IMAGE

--- js/script.js
+++ js/script.js
@@ -1,4 +1,40 @@
-$(body).click(function abc(){
-    document.write("faded");
-},
-abc());
+$(document).ready(function(){
+    $('.toggle1').click(function(){
+        $('.toggle1').toggle();
+    });
+    $('.toggle2').click(function(){
+        $('.toggle2').toggle();
+    });
+    $('.toggle3').click(function(){
+        $('.toggle3').toggle();
+    });
+    $(".port1").hover(function(){
+        $(".port1").css({
+            opacity:0.1
+        });$('.text2').css({
+            
+        })
+    },function(){
+        $(".port1").css({
+            opacity:1
+        });
+    });
+});
+
+// $(document).ready(function(){
+   
+// });
+// $(document).ready(function(){
+    
+// });
+// $(document).ready(fu(){
+   
+// $(document).ready(function() {
+
+//     $('.gallery-item').hover(function() {
+//       $(this).find('.img-title').fadeIn(300);
+//     }, function() {
+//       $(this).find('.img-title').fadeOut(100);
+//     });
+
+//   });
